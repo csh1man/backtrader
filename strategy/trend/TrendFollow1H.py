7c789c8bdf0a53bc89b26dafe9fcd6bb8c177f9b
csh1man <tjgusyt@gmail.com>
2025-02-28 17:30:48 +0900

    add etherium short

---
+++ strategy/trend/TrendFollow1H.py
@@ -0,0 +1,489 @@
+import backtrader as bt
+import pandas as pd
+import quantstats as qs
+from util.Util import DataUtil
+from decimal import Decimal
+
+pairs = {
+    'ETHUSDT': DataUtil.CANDLE_TICK_1HOUR,
+}
+
+leverage=4
+
+class TrendFollowV2(bt.Strategy):
+    params = dict(
+        risk={
+            'ETHUSDT':{
+                'long': Decimal('2'),
+                'short': Decimal('1')
+            },
+            '1000PEPEUSDT': [Decimal('1'), Decimal('2'), Decimal('4'), Decimal('8'), Decimal('16')],
+            'SUIUSDT': [Decimal('1'), Decimal('2'), Decimal('4'), Decimal('8'), Decimal('16')],
+            '1000BONKUSDT': [Decimal('1'), Decimal('2'), Decimal('4'), Decimal('8'), Decimal('16')],
+            'XRPUSDT': [Decimal('0.1'), Decimal('0.5'), Decimal('1'), Decimal('2'), Decimal('4'), Decimal('8'), Decimal('16')],
+            'DOGEUSDT': [Decimal('0.1'), Decimal('0.5'), Decimal('1'), Decimal('2'), Decimal('4'), Decimal('8'),Decimal('16')],
+        },
+        entry_mode={
+          'ETHUSDT' : 2
+        },
+        high_band_length={
+            'ETHUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            '1000PEPEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'SUIUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            '1000BONKUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'XRPUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'DOGEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+        },
+        low_band_length={
+            'ETHUSDT': {
+                'long': 150,
+                'short': 230
+            },
+            '1000PEPEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'SUIUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            '1000BONKUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'XRPUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'DOGEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+        },
+        high_band_constant={
+            'ETHUSDT': {
+                'long': -10,
+                'short': 55,
+            },
+            '1000PEPEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'SUIUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            '1000BONKUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'XRPUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'DOGEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+        },
+        low_band_constant={
+            'ETHUSDT': {
+                'long': 40,
+                'short': 5
+            },
+            '1000PEPEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'SUIUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            '1000BONKUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'XRPUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+            'DOGEUSDT': {
+                'long': 200,
+                'short': 30,
+            },
+        },
+        ma_length={
+          'ETHUSDT': {
+              'long' : [5, 10, 15],
+              'short' : [10, 20, 30]
+          },
+          '1000PEPEUSDT': {
+              'long': [5, 10, 15],
+              'short': [10, 20, 30]
+          },
+          'SUIUSDT': {
+              'long': [5, 10, 15],
+              'short': [10, 20, 30]
+          },
+            
+        },
+        rsi_limit={
+            '1000PEPEUSDT': 70,
+            'SUIUSDT': 70,
+            '1000BONKUSDT': 70,
+            'XRPUSDT': 70,
+            'DOGEUSDT': 70
+        },
+        bb_length={
+            '1000PEPEUSDT': 30,
+            'SUIUSDT': 30,
+            '1000BONKUSDT': 30,
+            'XRPUSDT': 30,
+            'DOGEUSDT': 30
+        },
+        bb_mult={
+            '1000PEPEUSDT': 0.5,
+            'SUIUSDT': 0.5,
+            '1000BONKUSDT': 0.5,
+            'XRPUSDT': 0.5,
+            'DOGEUSDT': 0.5,
+        },
+        exit_percent={
+            '1000PEPEUSDT': Decimal('1'),
+            'SUIUSDT': Decimal('1'),
+            '1000BONKUSDT': Decimal('1'),
+            'XRPUSDT': Decimal('1'),
+            'DOGEUSDT': Decimal('1'),
+        },
+        percent={
+            '1000PEPEUSDT': {
+                'bull': [Decimal('3.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15.0'), Decimal('20.0')],
+                'def': [Decimal('2.0'), Decimal('4.0'), Decimal('6.0'), Decimal('8.0'), Decimal('12')],
+                'bear': [Decimal('3.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15.0'), Decimal('20.0')],
+            },
+            'SUIUSDT': {
+                'bull': [Decimal('3.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15.0'), Decimal('20.0')],
+                'def': [Decimal('2.0'), Decimal('4.0'), Decimal('6.0'), Decimal('8.0'), Decimal('12')],
+                'bear': [Decimal('3.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15.0'), Decimal('20.0')],
+            },
+            '1000BONKUSDT': {
+                'bull': [Decimal('3.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15.0'), Decimal('20.0')],
+                'def': [Decimal('2.0'), Decimal('4.0'), Decimal('6.0'), Decimal('8.0'), Decimal('12')],
+                'bear': [Decimal('3.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15.0'), Decimal('20.0')],
+            },
+            'XRPUSDT': {
+                'bull': [Decimal('2.0'), Decimal('4.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15'),
+                         Decimal('20.0'), Decimal('25.0')],
+                'def': [Decimal('2.0'), Decimal('4.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15'), Decimal('20.0'),
+                        Decimal('25.0')],
+                'bear': [Decimal('5.0'), Decimal('7.0'), Decimal('10.0'), Decimal('15.0'), Decimal('20.0'),
+                         Decimal('25.0'), Decimal('30.0')],
+            },
+            'DOGEUSDT': {
+                'bull': [Decimal('4.0'), Decimal('6.0'), Decimal('8.0'), Decimal('12'), Decimal('16.0'),
+                         Decimal('20.0'), Decimal('25.0')],
+                'def': [Decimal('2.0'), Decimal('4.0'), Decimal('6.0'), Decimal('12.0'), Decimal('15'), Decimal('20.0'),
+                        Decimal('25.0')],
+                'bear': [Decimal('5.0'), Decimal('7.0'), Decimal('10.0'), Decimal('15.0'), Decimal('20.0'),
+                         Decimal('25.0'), Decimal('30.0')],
+            },
+        },
+        tick_size={
+            'ETHUSDT': {
+                DataUtil.COMPANY_BINANCE : Decimal('0.01'),
+            },
+            '1000PEPEUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.0000001'),
+                DataUtil.COMPANY_BYBIT: Decimal('0.0000010')
+            },
+            'SUIUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.000100'),
+                DataUtil.COMPANY_BYBIT: Decimal('0.00010')
+            },
+            '1000BONKUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.0000010'),
+                DataUtil.COMPANY_BYBIT: Decimal('0.00010')
+            },
+            'XRPUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.0001'),
+                DataUtil.COMPANY_BYBIT: Decimal('0.0001')
+            },
+            'DOGEUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.000010'),
+                DataUtil.COMPANY_BYBIT: Decimal('0.00001')
+            },
+        },
+        step_size={
+            'ETHUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.001'),
+            },
+            '1000PEPEUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('1'),
+                DataUtil.COMPANY_BYBIT: Decimal('100')
+            },
+            'SUIUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.1'),
+                DataUtil.COMPANY_BYBIT: Decimal('10')
+            },
+            '1000BONKUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('1'),
+                DataUtil.COMPANY_BYBIT: Decimal('10')
+            },
+            'XRPUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('0.1'),
+                DataUtil.COMPANY_BYBIT: Decimal('0.01')
+            },
+            'DOGEUSDT': {
+                DataUtil.COMPANY_BINANCE: Decimal('1'),
+                DataUtil.COMPANY_BYBIT: Decimal('1')
+            },
+        }
+    )
+
+    def log(self, txt):
+        print(f'{txt}')
+
+    def __init__(self):
+        self.names = []
+        self.pairs = []
+        self.opens = []
+        self.highs = []
+        self.lows = []
+        self.closes = []
+        self.dates = []
+        self.short_ma = []
+        self.mid_ma = []
+        self.long_ma = []
+
+        self.long_high_bands = []
+        self.long_low_bands = []
+
+        self.short_high_bands = []
+        self.short_low_bands = []
+
+        self.short_short_ma = []
+        self.short_mid_ma = []
+        self.short_long_ma = []
+
+        self.long_short_ma = []
+        self.long_mid_ma = []
+        self.long_long_ma = []
+
+        # 자산 기록용 변수 셋팅
+        self.order = None
+        self.date_value = []
+        self.my_assets = []
+
+        # 승률 계산을 위함
+        self.order_balance_list = []
+        self.initial_asset = self.broker.getvalue()
+        self.return_rate = 0
+        self.total_trading_count = 0
+        self.winning_trading_count = 0
+        self.winning_rate = 0
+
+        for i in range(0, len(self.datas)):
+            self.names.append(self.datas[i]._name)
+            self.pairs.append(self.datas[i])
+
+            self.opens.append(self.datas[i].open)
+            self.highs.append(self.datas[i].high)
+            self.lows.append(self.datas[i].low)
+            self.closes.append(self.datas[i].close)
+            self.dates.append(self.datas[i].datetime)
+
+        for i in range(0, len(self.pairs)):
+            name = self.names[i]
+
+            long_high_band = bt.indicators.Highest(self.highs[i], period=self.p.high_band_length[name]['long'])
+            long_low_band = bt.indicators.Lowest(self.lows[i], period=self.p.low_band_length[name]['long'])
+            self.long_high_bands.append(long_high_band)
+            self.long_low_bands.append(long_low_band)
+
+            short_high_band = bt.indicators.Highest(self.highs[i], period=self.p.high_band_length[name]['short'])
+            short_low_band = bt.indicators.Lowest(self.lows[i], period=self.p.low_band_length[name]['short'])
+            self.short_high_bands.append(short_high_band)
+            self.short_low_bands.append(short_low_band)
+
+            short_short_ma = bt.indicators.ExponentialMovingAverage(self.closes[i], period=self.p.ma_length[name]['short'][0])
+            short_mid_ma = bt.indicators.ExponentialMovingAverage(self.closes[i], period=self.p.ma_length[name]['short'][1])
+            short_long_ma = bt.indicators.ExponentialMovingAverage(self.closes[i], period=self.p.ma_length[name]['short'][2])
+
+            self.short_short_ma.append(short_short_ma)
+            self.short_mid_ma.append(short_mid_ma)
+            self.short_long_ma.append(short_long_ma)
+
+            long_short_ma = bt.indicators.ExponentialMovingAverage(self.closes[i], period=self.p.ma_length[name]['long'][0])
+            long_mid_ma = bt.indicators.ExponentialMovingAverage(self.closes[i], period=self.p.ma_length[name]['long'][1])
+            long_long_ma = bt.indicators.ExponentialMovingAverage(self.closes[i], period=self.p.ma_length[name]['long'][2])
+
+            self.long_short_ma.append(long_short_ma)
+            self.long_mid_ma.append(long_mid_ma)
+            self.long_long_ma.append(long_long_ma)
+
+    def cancel_all(self, target_name=None):
+        open_orders = self.broker.get_orders_open()
+        for order in open_orders:
+            if target_name and order.data._name != target_name:
+                continue
+            if order.status in [bt.Order.Submitted, bt.Order.Accepted]:
+                self.broker.cancel(order)
+
+    def notify_order(self, order):
+        cur_date = f"{order.data.datetime.date(0)} {str(order.data.datetime.time(0)).split('.')[0]}"
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(f'{order.ref:<3} {cur_date} =>'
+                         f' [매수{order.Status[order.status]:^10}] 종목 : {order.data._name} \t'
+                         f'수량:{order.size} \t'
+                         f'가격:{order.created.price:.4f}')
+                self.total_trading_count += 1
+            elif order.issell():
+                self.log(f'{order.ref:<3} {cur_date} =>'
+                         f' [매도{order.Status[order.status]:^10}] 종목 : {order.data._name} \t'
+                         f'수량:{order.size} \t'
+                         f'가격:{order.created.price:.4f}\n')
+
+
+    def stop(self):
+        self.log(f'전체 트레이딩 횟수 : {self.total_trading_count}')
+
+    def record_asset(self):
+        account_value = self.broker.get_cash()  # 현재 현금(보유 포지션 제외) 자산의 가격을 획득
+        broker_leverage = self.broker.comminfo[None].p.leverage  # cerebro에 설정한 레버리지 값 -> setcommission
+        position_value = 0.0
+        bought_value = 0.0
+        for pair in self.pairs:
+            position_value += self.getposition(pair).size * pair.low[0]
+            bought_value += self.getposition(pair).size * self.getposition(
+                pair).price  # 진입한 수량 x 평단가 즉, 현재 포지션 전체 가치를 의미(현금 제외)
+
+        account_value += position_value - bought_value * (broker_leverage - 1) / broker_leverage
+        self.order_balance_list.append([self.dates[0].datetime(0), account_value])
+        self.date_value.append(self.dates[0].datetime(0))
+        position_value = self.broker.getvalue()
+        for i in range(1, len(self.datas)):
+            position_value += self.getposition(self.datas[i]).size * self.lows[i][0]
+
+        self.my_assets.append(position_value)
+
+    def next(self):
+        self.record_asset()
+
+        for i in range(0, len(self.pairs)):
+            name = self.names[i]
+            self.cancel_all(target_name=name)
+
+        for i in range(0, len(self.pairs)):
+            name = self.names[i]
+
+            long_high_band = DataUtil.convert_to_decimal(self.long_high_bands[i][0])
+            long_low_band = DataUtil.convert_to_decimal(self.long_low_bands[i][0])
+
+            long_adj_high_band = long_high_band - (long_high_band-long_low_band) * (self.p.high_band_constant[name]['long'] / Decimal('100'))
+            long_adj_low_band = long_low_band + (long_high_band-long_low_band) * (self.p.low_band_constant[name]['long'] / Decimal('100'))
+
+            short_high_band = DataUtil.convert_to_decimal(self.short_high_bands[i][0])
+            short_low_band = DataUtil.convert_to_decimal(self.short_low_bands[i][0])
+
+            short_adj_high_band = short_high_band - (short_high_band-short_low_band) * (self.p.high_band_constant[name]['short'] / Decimal('100'))
+            short_adj_low_band = short_low_band + (short_high_band-short_low_band) * (self.p.low_band_constant[name]['short'] / Decimal('100'))
+
+            entry_mode = self.p.entry_mode[name]
+            equity = DataUtil.convert_to_decimal(self.broker.getvalue())
+            current_position_size = self.getposition(self.pairs[i]).size
+            if current_position_size == 0:
+                if entry_mode in [0, 2]:
+                    long_qty = equity * self.p.risk[name]['long'] / Decimal('100') / abs(long_adj_high_band-long_adj_low_band)
+                    long_qty = int(long_qty / self.p.step_size[name]) * self.p.step_size[name]
+                    if long_qty > 0:
+                        self.order = self.buy(exectype=bt.Order.Stop, data=self.pairs[i], price=float(long_adj_high_band), size=float(long_qty))
+                if entry_mode in [1, 2]:
+                    short_qty = equity * self.p.risk[name]['short'] / Decimal('100') / abs(short_adj_low_band-short_adj_high_band)
+                    short_qty = int(short_qty / self.p.step_size[name]) * self.p.step_size[name]
+                    if short_qty > 0:
+                        self.order = self.sell(exectype=bt.Order.Stop, data=self.pairs[i], price=float(short_adj_low_band), size=float(short_qty))
+            elif current_position_size > 0:
+                self.order = self.sell(exectype=bt.Order.Stop, data=self.pairs[i], price=float(long_adj_low_band), size=float(current_position_size))
+            elif current_position_size < 0:
+                self.order = self.buy(exectype=bt.Order.Stop, data=self.pairs[i], price=float(short_adj_high_band), size=float(abs(current_position_size)))
+
+
+
+if __name__ == '__main__':
+    # data_path = "C:/Users/user/Desktop/개인자료/콤트/candleData"
+    data_path = "C:/Users/KOSCOM/Desktop/각종자료/개인자료/krInvestment/백테스팅데이터"
+    # data_path = "/Users/tjgus/Desktop/project/krtrade/backData"
+    cerebro = bt.Cerebro()
+    cerebro.addstrategy(TrendFollowV2)
+
+    cerebro.broker.setcash(13000)
+    cerebro.broker.setcommission(commission=0.001, leverage=leverage)
+    cerebro.addanalyzer(bt.analyzers.PyFolio, _name='pyfolio')
+
+    for pair, tick_kind in pairs.items():
+        df = DataUtil.load_candle_data_as_df(data_path, DataUtil.COMPANY_BINANCE, pair, tick_kind)
+        data = bt.feeds.PandasData(dataname=df, datetime='datetime')
+        cerebro.adddata(data, name=pair)
+
+    before_balance = cerebro.broker.getvalue()
+    print('Before Portfolio Value: %.2f' % before_balance)
+    results = cerebro.run()
+    strat = results[0]
+    after_balance = cerebro.broker.getvalue()
+    print('Final Portfolio Value: %.2f' % after_balance)
+
+    # 수익률 계산
+    return_percentage = ((after_balance - before_balance) / before_balance) * 100
+    print('Strategy Return: %.2f%%' % return_percentage)
+
+    pyfoliozer = strat.analyzers.getbyname('pyfolio')
+    returns, positions, transactions, gross_lev = pyfoliozer.get_pf_items()
+    returns.index = returns.index.tz_convert(None)
+
+    print(f'strat.my_assets type :{type(strat.my_assets)}')
+    asset_list = pd.DataFrame({'asset': strat.my_assets}, index=pd.to_datetime(strat.date_value))
+    order_balance_list = strat.order_balance_list
+    mdd = qs.stats.max_drawdown(returns)
+    print(f" quanstats's my returns MDD : {mdd * 100:.2f} %")
+
+
+    file_name = "C:/Users/KOSCOM\Desktop/각종자료/개인자료/krInvestment/백테스팅데이터/결과/"
+    # file_name = "/Users/tjgus/Desktop/project/krtrade/backData/result/"
+    # file_name = "C:/Users/user/Desktop/개인자료/콤트/백테스트결과/"
+    for pair, tick_kind in pairs.items():
+        file_name += pair + "-"
+    file_name += "TrendFollowV2"
+
+    strat = results[0]
+    order_balance_list = strat.order_balance_list
+    df = pd.DataFrame(order_balance_list, columns=["date", "value"])
+    df['date'] = pd.to_datetime(df['date'])
+    df['date'] = df['date'].dt.date
+    df = df.sort_values('value', ascending=True).drop_duplicates('date').sort_index()
+    df['value'] = df['value'].astype('float64')
+    df['value'] = df['value'].pct_change()
+    df['date'] = pd.to_datetime(df['date'])
+    df = df.dropna()
+    df = df.set_index('date')
+    df.index.name = 'date'
+    df.to_csv(f'{file_name}.csv')
+    qs.reports.html(df['value'], output=f"{file_name}.html", download_filename=f"{file_name}.html", title=file_name)
